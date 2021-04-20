8ed6d3e113ecefe9891e106a02fbc2b62184dc0d
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-04-20 16:01:41 -0500

    Routes component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import { HashRouter, Switch, Route, BrowserRouter as Router, BrowserRouter} from 'react-router-dom'
-import Nav from './components/Nav/Nav'
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import HowItWorks from './components/HowItWorks/HowItWorks'
-import GroupCarousel from './components/Groups/Groups'
-import EventCarousel from './components/Events/Events'
-import Footer from './components/Footer/Footer'
+import { HashRouter, Switch, Route, BrowserRouter } from 'react-router-dom'
+
+import Routes from './components/Routes/Routes'
 
 const App = () => {
   return (
    
     <HashRouter basename="/">
-
-    <Nav />
-    <Home />
-    <About />
-    <HowItWorks />
-    <GroupCarousel />
-    <EventCarousel />
-    <Footer />
-        
-    
-      {/* <Switch>
-        <Route exact path="/"> <Home /> </Route>
-        <Route exact path="/"> <About /> </Route>
-      </Switch> */}
+      <Switch>
+        <Route path="/" exact> <Routes /> </Route>   
+      </Switch>
     </HashRouter>
   );
 }
---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import Home from '../Home/Home'
+import About from '../About/About'
+import HowItWorks from '../HowItWorks/HowItWorks'
+import GroupCarousel from '../Groups/Groups'
+import EventCarousel from '../Events/Events'
+import Footer from '../Footer/Footer'
+
+const Routes = () => {
+    return (
+        <div>
+            <Nav />
+            <Home />
+            <About />
+            <HowItWorks />
+            <GroupCarousel />
+            <EventCarousel />
+            <Footer />
+        </div>
+    )
+}
+
+export default Routes
